4f6c2ef15def1bd075c3635198c624c1f6666f13
Maciej <maciejrudnicki@outlook.com>
2022-12-11 22:00:49 +0100

    added state

--- react-complete-guide/src/components/Adoptions/AdoptionItem.js
+++ react-complete-guide/src/components/Adoptions/AdoptionItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AdoptionDate from './AdoptionDate';
 import Card from '../UI/Card';
@@ -6,14 +6,20 @@ import './AdoptionItem.css';
 
 const AdoptionItem = (props) => {
 
+  const [name, setName] = useState(props.title);
+
+  const handleClick = ()=> {
+    console.log("click")
+    setName((prev)=> prev + " 1");
+  }
   return (
     <Card className='adoption-item'>
       <AdoptionDate date={props.birthdate} />
       <div className='adoption-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{name}</h2>
         <div className='adoption-item__price'>{props.status}</div>
       </div>
-      <button>Change button</button>
+      <button onClick={(handleClick)}>Change button</button>
     </Card>
   );
 }
