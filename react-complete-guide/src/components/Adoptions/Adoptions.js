2880615089c01489305474b14e627d9b17cee5d6
Maciej <maciejrudnicki@outlook.com>
2022-12-17 23:11:07 +0100

    add filtering and conditional rendering

--- react-complete-guide/src/components/Adoptions/Adoptions.js
+++ react-complete-guide/src/components/Adoptions/Adoptions.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import AdoptionsFilter from "./AdoptionsFilter";
 import AdoptionItem from "./AdoptionItem";
 import Card from "../UI/Card";
 import "./Adoptions.css";
+import AdoptionStatus from "../../common/enums";
 
 const Adoptions = (props) => {
+  const [filteredAdoptions, setFilteredAdoptions] = useState(
+    props.adoptions.filter((a) => {
+      return a.status.id === AdoptionStatus.adopted.id;
+    })
+  );
 
   const onAdoptionsFiltedChange = (e) => {
-    console.log("filter! " + e.target.value)
-  }
+    setFilteredAdoptions(() => {
+      return props.adoptions.filter((a) => {
+        return a.status.id === e.target.value;
+      });
+    });
+  };
   return (
     <Card className="adoptions">
-      <AdoptionsFilter onAdoptionsFiltedChange = {onAdoptionsFiltedChange}/>
-      <AdoptionItem
-        title={props.items[0].title}
-        status={props.items[0].status}
-        birthdate={props.items[0].birthdate}
+      <AdoptionsFilter
+        onAdoptionsFiltedChange={onAdoptionsFiltedChange}
+        initialValue={AdoptionStatus.adopted}
       />
-      <AdoptionItem
-        title={props.items[1].title}
-        status={props.items[1].status}
-        birthdate={props.items[1].birthdate}
-      />
-      <AdoptionItem
-        title={props.items[2].title}
-        status={props.items[2].status}
-        birthdate={props.items[2].birthdate}
-      />
+      {filteredAdoptions.length === 0 && <p> Not adoptions in this status</p>}
+      {filteredAdoptions.length > 0 && filteredAdoptions.map((a) => (
+        <AdoptionItem
+          key={a.id}
+          title={a.title}
+          status={a.status.name} 
+          birthdate={a.birthdate}
+        />
+      ))}
+  
     </Card>
   );
 };
