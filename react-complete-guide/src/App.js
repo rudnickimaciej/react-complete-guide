5ee7f6a8ddfe6db42e5988e1337f5131166f0631
Maciej <maciejrudnicki@outlook.com>
2023-01-13 22:02:24 +0100

    init

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
+function App() {
+  const [movies, setMovies] = useState([]);
 
-const App = () => {
- 
+  const onFetchMoviesHandler = () => {
+    fetch("https://swapi.py4e.com/api/films")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movie) => {
+          return {
+            id: movie.episode_id,
+            title: movie.title,
+            openingText: movie.opening_crawl,
+            releaseDate: movie.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  };
+
+  async function onFetchMoviesHandlerAsyncAwaitVersion() {
+    const response = await fetch("https://swapi.py4e.com/api/films");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={onFetchMoviesHandler}> Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
-};
+}
 
 export default App;
